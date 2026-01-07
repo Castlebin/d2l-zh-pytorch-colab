542f79a800135c3b94515abd734829e335a959aa
Castlebin <Castlebin@gmail.com>
2026-01-07 21:53:15 +0800

    3.7_softmax回归的简洁实现.py

--- 0_my_code/03_线性神经网络/code/3.7_softmax回归的简洁实现.py
+++ 0_my_code/03_线性神经网络/code/3.7_softmax回归的简洁实现.py
@@ -121,8 +121,8 @@ train_losses_c, train_accs_c, test_accs_c = train(
 # %%
 # 可视化训练过程
 import matplotlib.pyplot as plt
-from dl_d2l.util import enable_matplotlib_chinese
-enable_matplotlib_chinese()
+from matplotlib_cn import matplotlib_util
+matplotlib_util.enable_chinese()
 
 fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 4))
 
