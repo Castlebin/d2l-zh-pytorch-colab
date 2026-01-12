f90918ca178a307d9ef16ea8f2335521b8d5f610
Castlebin <Castlebin@gmail.com>
2026-01-12 11:57:54 +0800

    7.2. 使用块的网络（VGG）

---
+++ 0_my_code/07_现代卷积神经网络/7.2_使用块的网络.py
@@ -0,0 +1,87 @@
+# @title 7.2. 使用块的网络（VGG）
+
+# %% 7.2.1. VGG 块
+import torch
+from torch import nn
+
+
+def vgg_block(num_convs, in_channels, out_channels):
+    """
+    :param num_convs: 卷积层的数量
+    :param in_channels: 输入通道的数量
+    :param out_channels: 输出通道的数量
+    :return: VGG block
+    """
+    layers = []
+    for _ in range(num_convs):
+        layers.append(nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1))
+        layers.append(nn.ReLU())
+        in_channels = out_channels
+
+    layers.append(nn.MaxPool2d(kernel_size=2, stride=2))
+    return nn.Sequential(*layers)
+
+
+# %% 7.2.2. VGG网络
+conv_arch = ((1, 64), (1, 128), (2, 256), (2, 512), (2, 512))
+
+
+def vgg(conv_arch):
+    conv_blks = []
+    in_channels = 1
+    # 卷积层部分
+    for (num_convs, out_channels) in conv_arch:
+        conv_blks.append(vgg_block(num_convs, in_channels, out_channels))
+        in_channels = out_channels
+
+    return nn.Sequential(
+        *conv_blks, nn.Flatten(),
+        # 全连接层部分
+        nn.Linear(out_channels * 7 * 7, 4096), nn.ReLU(), nn.Dropout(0.5),
+        nn.Linear(4096, 4096), nn.ReLU(), nn.Dropout(0.5),
+        nn.Linear(4096, 10))
+
+
+net = vgg(conv_arch)
+
+# %% 构建一个高度和宽度为 224 的单通道数据样本，以观察每个层输出的形状
+X = torch.randn(size=(1, 1, 224, 224))
+for blk in net:
+    X = blk(X)
+    print(blk.__class__.__name__, 'output shape:\t', X.shape)
+
+# %% 使用 torchinfo 观察模型
+import torchinfo
+
+torchinfo.summary(net)
+
+# %% 7.2.3. 训练模型
+'''由于VGG-11比AlexNet计算量更大，因此我们构建了一个通道数较少的网络，足够用于训练Fashion-MNIST数据集。'''
+ratio = 4
+small_conv_arch = [(pair[0], pair[1] // ratio) for pair in conv_arch]
+net = vgg(small_conv_arch)
+
+torchinfo.summary(net)
+
+# %% 训练
+from dl_d2l import d2l_torch as d2l
+from dl_d2l.util import colab_util, device_util
+import os
+from matplotlib import pyplot as plt
+
+base_data_dir = colab_util.get_base_data_dir()
+print(f'base data dir: {base_data_dir}')
+
+datasets_dir = os.path.join(base_data_dir, 'ML', 'Datasets')
+os.makedirs(datasets_dir, exist_ok=True)
+print(f'datasets dir: {datasets_dir}')
+
+# 获取可用的设备
+device = device_util.get_available_device()
+
+# 训练模型
+lr, num_epochs, batch_size = 0.05, 10, 128
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224, root_dir=datasets_dir)
+d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, device)
+
+plt.show()
